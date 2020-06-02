2cc8c848d6ea0f6c2003f4e0a507c3a8b6f07c3f
mschan73 <shingshing125@gmail.com>
2020-06-03 03:27:42 +0800

    Bootstrap on Heroku

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const PORT= process.env.PORT || 5000;
+var path = require('path');
 app.listen(PORT, () => {
     console.log('App listening on port 5000!');
 });
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
+
 
 app.use(express.urlencoded({extended:false}))
 
